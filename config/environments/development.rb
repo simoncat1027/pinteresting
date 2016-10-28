3a33667ad7a8dd3a05604535f70228c75c62004a
Nick.Xu <simoncat1989@gmail.com>
2016-10-28 10:37:59 +1000

    add image upload with paper clip

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,3 +41,5 @@ Rails.application.configure do
   
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
+
+Paperclip.options[:command_path] = "C:/Program Files/ImageMagick-7.0.3-Q16/"
